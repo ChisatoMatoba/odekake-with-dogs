d56e1ff30515fd712bb00ff0e80bc366ea69989d
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-08-23 20:00:47 +0900

    ビュー作成(ｱｸﾃｨﾌﾞﾊｯｼｭ以外)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many   :conditions, through: :post_conditions
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
