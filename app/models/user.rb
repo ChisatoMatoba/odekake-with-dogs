0768d2b30fd62f0cf98440e6735fc0ed0642d7ad
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-08-24 17:53:20 +0900

    コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :posts
+  has_many :comments
+
   validates :nickname,           presence: true
   validates :birthday,           presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
