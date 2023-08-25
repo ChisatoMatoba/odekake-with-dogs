2a19cca5ab4ad2bd2d6cb56d016ef647faf33c48
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-08-25 11:05:20 +0900

    ユーザーの年代を計算して表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,31 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  # ユーザーの年代を計算
+  require 'happybirthday'
+  def age_group
+    birthday = Happybirthday.born_on(self.birthday)
+    case birthday.age.years_old
+    when 0..9
+      "0〜9歳"
+    when 10..19
+      "10〜19歳"
+    when 20..29
+      "20〜29歳"
+    when 30..39
+      "30〜39歳"
+    when 40..49
+      "40〜49歳"
+    when 50..59
+      "50〜59歳"
+    when 60..69
+      "60〜69歳"
+    when 70..Float::INFINITY
+      "70歳以上"
+    else
+      "年齢非公開"
+    end
+  end
+
 end
