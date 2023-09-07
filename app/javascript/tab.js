6f39bcc426b520ac8ba587cbef4b9a03b2868a2f
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-09-07 18:00:16 +0900

    タブ表示改善

--- app/javascript/tab.js
+++ app/javascript/tab.js
@@ -10,20 +10,6 @@ function handleTabClick(e) {
   $(targetId).addClass('show active');
 }
 
-// 画像がクリックされたときの処理関数を定義
-function handleImageClick(e) {
-  e.preventDefault(); // リンクのデフォルトの動作を無効にする
-
-  // クリックされた画像に関連するモーダルのIDを取得
-  var targetModalId = $(this).attr('data-bs-target-modal');
-  console.log(targetModalId)
-
-  // クリックされた画像に関連するモーダルを表示
-  $(targetModalId).modal('show');
-}
-
-// 画像にクリックイベントをバインド
-$('.gallery-image').on('click', handleImageClick);
 
 // ページ読み込み時にクリックイベントをバインド
 $(document).ready(function() {
