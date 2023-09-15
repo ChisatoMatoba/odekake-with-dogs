60e8ae60651c3deabbbe86494c276b66731497d7
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-09-15 13:13:17 +0900

    テストOK

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 { Faker::Internet.email }
+    password              { 'a1A' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    nickname              { Faker::Name.initials(number: 2) }
+    birthday              { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
