e52734857e0a7162a700477defbf144ca14bc1db
chisatom0908 <139729122+chisatom0908@users.noreply.github.com>
2023-09-24 12:46:07 +0900

    factorybot

--- spec/factories/dogs.rb
+++ spec/factories/dogs.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :dog do
-    dog_name      
-    dog_birthday  
-    breed_id      
-    gender_id     
+    dog_name      { Faker::Name.initials(number: 2) }
+    dog_birthday  { Faker::Date.birthday(max_age: 20) }
+    breed         { Breed.find_by(name: 'コーギー(ウェルシュコーギーカーディガン)') }
+    gender        { Gender.find_by(name: '男の子♂') }
+    association :user
+
+    after(:build) do |dog|
+      dog.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @dog = FactoryBot.build(:dog)
+  end
+
+  describe '愛犬登録' do
+    context '愛犬登録ができる場合' do
+      it 'すべての入力内容とimageが存在していれば保存できる' do
+        expect(@dog).to be_valid
+      end
+    end
+
+    context '愛犬登録ができない場合' do
+      it '画像が選択されていなければ保存できない' do
+      end
+    end
+  end
 end
