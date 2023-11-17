7396037fa212bffdd95af0e960ae44446932e529
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-17 16:17:11 +0900

    施設条件activehash

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,23 @@
 class Facility < ApplicationRecord
   belongs_to :user
   has_many :posts
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+
+  serialize :condition_ids, Array
+
+  def conditions
+    FacilityCondition.find(condition_ids)
+  end
+
+  with_options numericality: { other_than: 1, message: "を入力してください" } do
+    validates :prefecture_id
+    validates :category_id
+  end
+
+  with_options presence: true do
+    validates :place_name
+  end
 end
