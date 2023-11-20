7b5d6daca489bec546525e4d1ec6b50a2e5a9606
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-20 18:10:17 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
-  resources :posts do
-    resources :comments, only: :create
+  resources :facilities do
+    resources :posts do
+      resources :comments, only: :create
+    end
   end
-  resources :users, only: :show do
+    resources :users, only: :show do
     resources :dogs, only: [:new, :create]
   end
 end
