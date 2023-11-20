fa67e1a231decfa45d9b3f3189a5a6c9d885614f
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-20 11:48:04 +0900

    condition_idsのカラムを削除

--- db/migrate/20231117062146_create_facilities.rb
+++ db/migrate/20231117062146_create_facilities.rb
@@ -4,7 +4,6 @@ class CreateFacilities < ActiveRecord::Migration[7.1]
       t.integer    :prefecture_id, null: false
       t.string     :place_name,    null: false
       t.integer    :category_id,   null: false
-      t.text       :condition_ids
       t.references :user,          null: false, foreign_key: true
       t.timestamps
     end
--- db/migrate/20231120003035_remove_condition_ids_from_facilities.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConditionIdsFromFacilities < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :facilities, :condition_ids, :text
-  end
-end
