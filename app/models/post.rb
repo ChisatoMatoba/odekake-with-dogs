8bef16067be42000ba96c9c05399d0eb4fb39676
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-21 10:27:14 +0900

    地域分類を投稿から施設モデルへ移動

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,26 +25,4 @@ class Post < ApplicationRecord
     validates :review
     validates :images, length: { minimum: 1, maximum: 5, message: 'は1枚以上5枚以下にしてください' }
   end
-
-  # 投稿された地域を分類
-  def area_group
-    case prefecture_id
-    when 2
-      %w[北海道 nav-hokkaido-content nav-hokkaido-tab]
-    when 3..8
-      %w[東北 nav-tohoku-content nav-tohoku-tab]
-    when 9..15
-      %w[関東 nav-kanto-content nav-kanto-tab]
-    when 16..25
-      %w[中部 nav-chubu-content nav-chubu-tab]
-    when 26..31
-      %w[近畿 nav-kinki-content nav-kinki-tab]
-    when 32..40
-      %w[中国四国 nav-chushikoku-content nav-chushikoku-tab]
-    when 41..47
-      %w[九州 nav-kyushu-content nav-kyushu-tab]
-    when 48
-      %w[沖縄 nav-okinawa-content nav-okinawa-tab]
-    end
-  end
 end
