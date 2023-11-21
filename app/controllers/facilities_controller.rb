f0ba3685b5f0cc27538d961046b3c855c5cdb98b
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-21 15:14:20 +0900

    都道府県順に並べ替え

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -3,7 +3,7 @@ class FacilitiesController < ApplicationController
   before_action :set_facility, only: %i[edit show update destroy]
 
   def index
-    @facilities = Facility.includes(:user).order(created_at: :desc)
+    @facilities = Facility.order(:prefecture_id)
 
     @areas = {}
     # 投稿を地域ごとにグループ化する
