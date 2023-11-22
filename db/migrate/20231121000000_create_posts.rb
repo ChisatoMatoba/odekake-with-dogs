fac9d7e609437c75e3fad70f4331d45348b0aeeb
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-22 15:32:46 +0900

    前ブランチで間違ったmigrationしてしまっていたものをこちらでも修復

--- db/migrate/20231121000000_create_posts.rb
+++ db/migrate/20231121000000_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.integer    :prefecture_id, null: false
+      t.string     :place_name,    null: false
+      t.integer    :category_id,   null: false
       t.integer    :people_num,    null: false
       t.integer    :dogs_num,      null: false
       t.integer    :rating_id,     null: false
--- db/migrate/20231121000001_create_post_conditions.rb
+++ db/migrate/20231121000001_create_post_conditions.rb
@@ -1,6 +1,6 @@
-class DropPostConditions < ActiveRecord::Migration[7.1]
+class CreatePostConditions < ActiveRecord::Migration[7.0]
   def change
-    drop_table :post_conditions do |t|
+    create_table :post_conditions do |t|
       t.references :post,      null: false, foreign_key: true
       t.references :condition, null: false, foreign_key: true
       t.timestamps
