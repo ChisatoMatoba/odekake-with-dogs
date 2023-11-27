f51078dd5f07e4de04be96df7e42b411ef0c5f2f
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-27 10:33:33 +0900

    指摘点修正2 関数を別途宣言

--- app/javascript/facility_index.js
+++ app/javascript/facility_index.js
@@ -1,17 +1,24 @@
-document.addEventListener('turbo:load', function() {
+// 行をクリックした時の動作
+function handleRowClick() {
+  const prefectureId = this.dataset.prefectureId;
+  const facilityRow = document.querySelector('.facility-row[data-prefecture-id="' + prefectureId + '"]');
+  const isHidden = facilityRow.style.display === 'none';
+
+  // 施設リストの表示/非表示の切り替え
+  facilityRow.style.display = isHidden? 'table-row' : 'none';
+
+  // アイコンの切り替え
+  const icon = this.querySelector('.prefecture-icon');
+  icon.classList.toggle('bi-chevron-down');
+  icon.classList.toggle('bi-chevron-up');
+}
+
+// まとめ
+function handlePrefectureRowClick() {
   document.querySelectorAll('.prefecture-row').forEach(function(row) {
-    row.addEventListener('click', function() {
-      const prefectureId = this.dataset.prefectureId;
-      const facilityRow = document.querySelector('.facility-row[data-prefecture-id="' + prefectureId + '"]');
-      const isHidden = facilityRow.style.display === 'none';
-
-      // 施設リストの表示/非表示の切り替え
-      facilityRow.style.display = isHidden ? 'table-row' : 'none';
-
-      // アイコンの切り替え
-      const icon = this.querySelector('.prefecture-icon');
-      icon.classList.toggle('bi-chevron-down');
-      icon.classList.toggle('bi-chevron-up');
-    });
+    row.addEventListener('click', handleRowClick);
   });
-});
+}
+
+// ページロード時にイベントリスナー設定
+document.addEventListener('turbo:load', handlePrefectureRowClick);
