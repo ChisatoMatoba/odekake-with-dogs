eca196bf2f92a2c4d83815cfa89082fda6b5f7ea
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-27 16:00:32 +0900

    指摘点修正

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -7,7 +7,7 @@ class FacilitiesController < ApplicationController
 
     @prefectures = Prefecture.order(:id)
     @facilities = Facility.order(:prefecture_id)
-    @facilities_groupby_pref = Facility.all.group_by(&:prefecture_id)
+    @facilities_groupby_pref = @facilities.all.group_by(&:prefecture_id)
 
     @areas = {}
     # 投稿を地域ごとにグループ化する
