322dbefea662e5045f9a6b866afd95e518a1e1f3
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-27 10:09:42 +0900

    指摘点修正1 var→const

--- app/javascript/facility_index.js
+++ app/javascript/facility_index.js
@@ -1,15 +1,15 @@
 document.addEventListener('turbo:load', function() {
   document.querySelectorAll('.prefecture-row').forEach(function(row) {
     row.addEventListener('click', function() {
-      var prefectureId = this.dataset.prefectureId;
-      var facilityRow = document.querySelector('.facility-row[data-prefecture-id="' + prefectureId + '"]');
-      var isHidden = facilityRow.style.display === 'none';
+      const prefectureId = this.dataset.prefectureId;
+      const facilityRow = document.querySelector('.facility-row[data-prefecture-id="' + prefectureId + '"]');
+      const isHidden = facilityRow.style.display === 'none';
 
       // 施設リストの表示/非表示の切り替え
       facilityRow.style.display = isHidden ? 'table-row' : 'none';
 
       // アイコンの切り替え
-      var icon = this.querySelector('.prefecture-icon');
+      const icon = this.querySelector('.prefecture-icon');
       icon.classList.toggle('bi-chevron-down');
       icon.classList.toggle('bi-chevron-up');
     });
