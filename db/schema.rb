08e9a0beb91bfcc72e169b246d2751d079b7ce30
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-28 14:41:02 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_044130) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_053849) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -85,6 +85,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_044130) do
     t.index ["facility_id"], name: "index_facility_conditions_on_facility_id"
   end
 
+  create_table "post_tags", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.integer "people_num", null: false
     t.integer "dogs_num", null: false
@@ -96,6 +105,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_044130) do
     t.datetime "updated_at", null: false
     t.index ["facility_id"], name: "index_posts_on_facility_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tags", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -120,6 +135,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_044130) do
   add_foreign_key "facilities", "users"
   add_foreign_key "facility_conditions", "conditions"
   add_foreign_key "facility_conditions", "facilities"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "facilities"
   add_foreign_key "posts", "users"
 end
