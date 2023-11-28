72c378f77fb00199ae90b188dd1f78fe3b01848c
Chisato Matoba <chisato.matoba@luxiar.com>
2023-11-28 11:09:49 +0900

    user-show修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @age = @user.age_group
     @dog = Dog.new
-    @dogs = @user.dogs.includes(:user)
-    @posts = @user.posts.includes(:user)
+    @dogs = @user.dogs
+    @posts = @user.posts.includes(:facility, images_attachments: :blob)
   end
 end
