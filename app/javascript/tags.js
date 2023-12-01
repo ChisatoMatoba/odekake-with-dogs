c417152ecf5425b0fbb97e7d9de74c68cc9b4063
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-01 15:32:27 +0900

    エンターでタグを表示

--- app/javascript/tags.js
+++ app/javascript/tags.js
@@ -1,28 +1,44 @@
 document.addEventListener('turbo:load', function() {
-  const addButton = document.getElementById('addTagButton');
-  if (addButton) {
-    addButton.addEventListener('click', function() {
-      const inputText = document.getElementById('inputTag').value;
+  const inputTag = document.getElementById('inputTag');
+  const tagContainer = document.getElementById('TagContainer');
 
-      // 入力されたテキストをタグとして表示
-      if(inputText) {
-        const newBadge = document.createElement('div');
-        newBadge.textContent = inputText;
-        newBadge.classList.add('badge');
+  // タグを追加する関数
+  function addTag() {
+    const inputText = inputTag.value;
 
-        document.getElementById('TagContainer').appendChild(newBadge);
+    if(inputText) {
+      // タグを表示
+      const newBadge = document.createElement('div');
+      newBadge.textContent = inputText;
+      newBadge.classList.add('badge');
+      tagContainer.appendChild(newBadge);
 
-        // 隠しフィールドを追加
-        const hiddenInput = document.createElement('input');
-        hiddenInput.setAttribute('type', 'hidden');
-        hiddenInput.setAttribute('name', 'post[tags_attributes][][name]');
-        hiddenInput.value = inputText;
-        document.querySelector('form').appendChild(hiddenInput);
+      // 隠しフィールドを追加
+      const hiddenInput = document.createElement('input');
+      hiddenInput.setAttribute('type', 'hidden');
+      hiddenInput.setAttribute('name', 'post[tags_attributes][][name]');
+      hiddenInput.value = inputText;
+      document.querySelector('form').appendChild(hiddenInput);
 
-        // 入力フィールドをクリア
-        document.getElementById('inputTag').value = '';
+      // 入力フィールドをクリア
+      inputTag.value = '';
+    }
+  }
+
+  // 入力フィールドにユーザーが入力したとき
+  if (inputTag) {
+    inputTag.addEventListener('keypress', function(event) {
+      // エンターキー押下時の処理
+      if (event.key === 'Enter') {
+        event.preventDefault(); // フォームの送信を防ぐ
+        addTag();
       }
 
+      // ボタンクリック時の処理
+      const addButton = document.getElementById('addTagButton');
+      if (addButton) {
+        addButton.addEventListener('click', addTag);
+      }
     });
   }
 });
