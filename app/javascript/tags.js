1d4040f7814430e61b49e3fb41745664878b6e1b
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-01 15:49:23 +0900

    リファクタリングで整理

--- app/javascript/tags.js
+++ app/javascript/tags.js
@@ -1,44 +1,55 @@
 document.addEventListener('turbo:load', function() {
   const inputTag = document.getElementById('inputTag');
   const tagContainer = document.getElementById('TagContainer');
+  const addButton = document.getElementById('addTagButton');
 
   // タグを追加する関数
   function addTag() {
     const inputText = inputTag.value;
-
-    if(inputText) {
-      // タグを表示
-      const newBadge = document.createElement('div');
-      newBadge.textContent = inputText;
-      newBadge.classList.add('badge');
-      tagContainer.appendChild(newBadge);
-
-      // 隠しフィールドを追加
-      const hiddenInput = document.createElement('input');
-      hiddenInput.setAttribute('type', 'hidden');
-      hiddenInput.setAttribute('name', 'post[tags_attributes][][name]');
-      hiddenInput.value = inputText;
-      document.querySelector('form').appendChild(hiddenInput);
+    if (inputText) {
+      createBadge(inputText);
+      createHiddenInput(inputText);
 
       // 入力フィールドをクリア
       inputTag.value = '';
     }
   }
 
-  // 入力フィールドにユーザーが入力したとき
-  if (inputTag) {
-    inputTag.addEventListener('keypress', function(event) {
-      // エンターキー押下時の処理
-      if (event.key === 'Enter') {
-        event.preventDefault(); // フォームの送信を防ぐ
-        addTag();
-      }
-
-      // ボタンクリック時の処理
-      const addButton = document.getElementById('addTagButton');
-      if (addButton) {
-        addButton.addEventListener('click', addTag);
-      }
-    });
+  // タグを表示する関数
+  function createBadge(text) {
+    const badge = document.createElement('div');
+    badge.textContent = text;
+    badge.classList.add('badge');
+    tagContainer.appendChild(badge);
   }
+
+  // 隠しフィールドを追加する関数
+  function createHiddenInput(value) {
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'hidden';
+    hiddenInput.name = 'post[tags_attributes][][name]';
+    hiddenInput.value = value;
+    document.querySelector('form').appendChild(hiddenInput);
+  }
+
+  // イベントリスナー関数
+  function addEventListeners() {
+    // 入力フィールドにユーザーが入力したとき(エンターキー押下時の処理)
+    if (inputTag) {
+      inputTag.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+          event.preventDefault();
+          addTag();
+        }
+      });
+    }
+
+    // タグ追加ボタンクリック時の処理
+    if (addButton) {
+      addButton.addEventListener('click', addTag);
+    }
+  }
+
+  // イベントリスナー実行
+  addEventListeners();
 });
