573940216b7151df4589f11cc97354778b480c1f
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-05 11:29:52 +0900

    タグ詳細画面にギャラリー表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,6 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = Tag.find(params[:id])
+    @tag = Tag.includes(posts: [:facility, :user, { images_attachments: :blob }]).find_by(id: params[:id])
   end
 end
