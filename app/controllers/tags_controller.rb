db5badb37b2a9f8259dea39cb221def7bf036141
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-05 11:07:22 +0900

    ルーティング

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,8 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.left_joins(:posts).group(:id).order('COUNT(posts.id) DESC')
   end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
 end
