a599746fdae637f42649e6caf746a7e6dbf75676
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-11 13:50:05 +0900

    入力値をファイル内で作成に変更

--- spec/system/user_registration_spec.rb
+++ spec/system/user_registration_spec.rb
@@ -8,13 +8,21 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
   context 'ユーザー新規登録ができるとき' do
     # 正しいuser情報を作成
-    let(:user) { FactoryBot.build(:user) }
+    let(:valid_user_info) do
+      {
+        nickname: 'テストユーザー',
+        email: 'test@example.com',
+        password: 'password123',
+        password_confirmation: 'password123',
+        birthday: '2000-01-01'
+      }
+    end
 
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # 新規登録ページへ移動する
       move_to_registration
       # ユーザー情報を入力する
-      fill_in_user_info(user)
+      fill_in_user_info(valid_user_info)
       # サインアップボタンを押す
       find('input[name="commit"]').click
       # トップページへ遷移したことを確認する
@@ -31,13 +39,21 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
   context 'ユーザー新規登録ができないとき' do
     # 誤ったuser情報を作成
-    let(:user) { FactoryBot.build(:user, email: nil) }
+    let(:invalid_user_info) do
+      {
+        nickname: '',
+        email: '',
+        password: 'pass',
+        password_confirmation: 'password',
+        birthday: ''
+      }
+    end
 
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # 新規登録ページへ移動する
       move_to_registration
       # ユーザー情報を入力する
-      fill_in_user_info(user)
+      fill_in_user_info(invalid_user_info)
       # サインアップボタンを押す
       find('input[name="commit"]').click
       # 新規登録ページへ戻されることを確認する
@@ -60,13 +76,16 @@ RSpec.describe 'ユーザー新規登録', type: :system do
 
   def fill_in_user_info(user)
     # ユーザー情報を入力する
-    fill_in 'nickname', with: user.nickname
-    fill_in 'email', with: user.email
-    fill_in 'password', with: user.password
-    fill_in 'password-confirmation', with: user.password_confirmation
-    # 生年月日を入力する
-    select user.birthday.year.to_s, from: 'user_birthday_1i' # 年
-    select user.birthday.month.to_s, from: 'user_birthday_2i' # 月
-    select user.birthday.day.to_s, from: 'user_birthday_3i' # 日
+    fill_in 'nickname', with: user[:nickname]
+    fill_in 'email', with: user[:email]
+    fill_in 'password', with: user[:password]
+    fill_in 'password-confirmation', with: user[:password_confirmation]
+    # 生年月日が有効な場合のみ入力する
+    return if user[:birthday].blank?
+
+    birthday = Date.parse(user[:birthday])
+    select birthday.year.to_s, from: 'user_birthday_1i' # 年
+    select birthday.month.to_s, from: 'user_birthday_2i' # 月
+    select birthday.day.to_s, from: 'user_birthday_3i' # 日
   end
 end
