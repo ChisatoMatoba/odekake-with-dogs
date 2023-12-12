27279a7ac35b5c97211765de30bae229dca21fe1
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 13:14:33 +0900

    テスト用のユーザー作成

--- spec/system/facility_registration_spec.rb
+++ spec/system/facility_registration_spec.rb
@@ -2,20 +2,14 @@ require 'rails_helper'
 
 RSpec.describe '施設新規登録', type: :system do
   before do
-
+    # テスト用のユーザーを作成
+    @user = FactoryBot.create(:user)
   end
 
-  it '正しい情報を入力すれば' do
+  it '正しい情報を入力すれば施設を登録できて、施設詳細画面に移動する' do
 
   end
-
-  it 'ログイン中にはユーザーログアウトができてトップページに移動する' do
-
-  end
-
 
   private
-
-
 
 end
