973d3a291f1e2ccb88de2c95d23f51f817461942
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 13:45:45 +0900

    施設フォーム入力

--- spec/system/facility_registration_spec.rb
+++ spec/system/facility_registration_spec.rb
@@ -11,7 +11,24 @@ RSpec.describe '施設新規登録', type: :system do
     # 正しいユーザー情報を入力する
     fill_in 'email', with: user.email
     fill_in 'password', with: user.password
-    # ログインボタンを押す
+    # ログインボタンを押す(rootへ移動)
+    find('input[name="commit"]').click
+
+    # 施設一覧・新規投稿ボタンが表示されることを確認する
+    expect(page).to have_content('施設一覧・新規投稿')
+    # 施設一覧・新規投稿ボタンをクリックする
+    click_on '施設一覧・新規投稿'
+    # 施設一覧ページへ遷移することを確認する
+    expect(page).to have_current_path(facilities_path)
+    # 施設登録画面へボタンをクリックする
+    click_on '施設登録画面へ'
+    # 施設登録ページへ遷移することを確認する
+    expect(page).to have_current_path(new_facility_path)
+    # 施設情報を入力する
+    select '北海道', from: 'prefecture'
+    fill_in 'item-name', with: 'テスト施設'
+    select '宿泊', from: 'item-category'
+    # 登録ボタンを押す
     find('input[name="commit"]').click
   end
 end
