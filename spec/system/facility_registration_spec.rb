b46f2d594171f597f7864f9794ece188223f178d
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 10:08:24 +0900

    施設登録のspecファイル作成

---
+++ spec/system/facility_registration_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe '施設新規登録', type: :system do
+  before do
+
+  end
+
+  it '正しい情報を入力すれば' do
+
+  end
+
+  it 'ログイン中にはユーザーログアウトができてトップページに移動する' do
+
+  end
+
+
+  private
+
+
+
+end
