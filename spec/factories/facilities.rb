1748319c65ad0277eeedb8d4e18b41a1e11ccd1b
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 14:41:29 +0900

    施設のfactorybot作成

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :facility do
+    prefecture { Prefecture.find_by(name: '北海道') }
+    place_name { Faker::Name.initials(number: 2) }
+    category   { Category.find_by(name: '宿泊') }
+    association :user
   end
 end
