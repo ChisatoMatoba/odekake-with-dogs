a6bea47125cfadf86243280c7a020a4562f35f54
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 13:29:06 +0900

    ログイン処理

--- spec/system/facility_registration_spec.rb
+++ spec/system/facility_registration_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe '施設新規登録', type: :system do
-  before do
-    # テスト用のユーザーを作成
-    @user = FactoryBot.create(:user)
-  end
+  # テスト用のユーザーを作成
+  let!(:user) { FactoryBot.create(:user) }
 
   it '正しい情報を入力すれば施設を登録できて、施設詳細画面に移動する' do
-
+    # ログイン処理を行う
+    # ログインページへ遷移する
+    visit new_user_session_path
+    # 正しいユーザー情報を入力する
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    # ログインボタンを押す
+    find('input[name="commit"]').click
   end
-
-  private
-
 end
