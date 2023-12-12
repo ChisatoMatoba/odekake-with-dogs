f4f15b9619df911da9f88ff78a3a7dd63a1fca05
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-12 15:56:11 +0900

    編集画面へ遷移

--- spec/system/facility_edit_del_spec.rb
+++ spec/system/facility_edit_del_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe '施設新規登録', type: :system do
   # テスト用のユーザーを作成
   let!(:user) { FactoryBot.create(:user) }
   # テスト用の施設を作成
-  let!(:facility) { FactoryBot.create(:facility) }
+  let!(:facility) { FactoryBot.create(:facility, user: user) }
 
   before do
     # 事前にサインインしておく
@@ -12,6 +12,12 @@ RSpec.describe '施設新規登録', type: :system do
   end
 
   it '正しい情報を入力すれば施設を編集できて、施設詳細画面に移動する' do
+    # 施設詳細画面へ遷移する
+    visit facility_path(facility)
+    # 編集ボタンをクリックする
+    click_on '編集'
+    # 施設編集ページへ遷移することを確認する
+    expect(page).to have_current_path(edit_facility_path(facility))
   end
 
   it '自分が作成した施設を削除できる' do
