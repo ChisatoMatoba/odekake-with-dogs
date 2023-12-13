5cf792259621892ac71f0c4e93ae8795e57037f4
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-13 16:27:51 +0900

    タグあり投稿ができることを確認

--- spec/system/post_registration_spec.rb
+++ spec/system/post_registration_spec.rb
@@ -27,16 +27,61 @@ RSpec.describe '施設新規登録', type: :system do
     # 新規投稿ページへ移動する
     visit new_facility_post_path(facility)
     # 正しい投稿内容を入力する
+    fill_in_post_form
+    # 投稿ボタンを押す
+    click_on '投稿する'
+    # 登録した投稿の内容が投稿詳細ページに表示されていることを確認
+    expect(page).to have_text("#{user.nickname}さんのレビュー")
+    expect(page).to have_text('楽しかった')
+  end
+
+  it '正しい情報＋タグ1つを入力すれば投稿でき、投稿詳細画面に移動する' do
+    # 新規投稿ページへ移動する
+    visit new_facility_post_path(facility)
+    # 正しい投稿内容を入力する
+    fill_in_post_form
+    # タグを入力する
+    fill_in 'input-tag', with: 'タグ'
+    # タグを追加する
+    click_on 'タグを追加'
+    # 投稿ボタンを押す
+    click_on '投稿する'
+    # 登録した投稿の内容が投稿詳細ページに表示されていることを確認
+    expect(page).to have_text("#{user.nickname}さんのレビュー")
+    expect(page).to have_text('楽しかった')
+    expect(page).to have_text('タグ')
+  end
+
+  it '正しい情報＋タグ複数を入力すれば投稿でき、投稿詳細画面に移動する' do
+    # 新規投稿ページへ移動する
+    visit new_facility_post_path(facility)
+    # 正しい投稿内容を入力する
+    fill_in_post_form
+    # タグを入力する
+    fill_in 'input-tag', with: 'タグ'
+    # タグを追加する
+    click_on 'タグを追加'
+    # タグを入力する
+    fill_in 'input-tag', with: 'タグその2'
+    # タグを追加する
+    click_on 'タグを追加'
+    # 投稿ボタンを押す
+    click_on '投稿する'
+    # 登録した投稿の内容が投稿詳細ページに表示されていることを確認
+    expect(page).to have_text("#{user.nickname}さんのレビュー")
+    expect(page).to have_text('楽しかった')
+    expect(page).to have_text('タグ')
+    expect(page).to have_text('タグその2')
+  end
+
+  private
+
+  def fill_in_post_form
     fill_in 'post[people_num]', with: '3'
     fill_in 'post[dogs_num]', with: '3'
     select '★★★★☆', from: 'item-category'
     fill_in 'post[review]', with: '楽しかった'
     # 画像を添付する
     attach_file('post[images][]', Rails.root.join('public/images/test_image.png'))
-    # 投稿ボタンを押す
-    click_on '投稿する'
-    # 登録した投稿の内容が投稿詳細ページに表示されていることを確認
-    expect(page).to have_text("#{user.nickname}さんのレビュー")
-    expect(page).to have_text('楽しかった')
   end
 end
