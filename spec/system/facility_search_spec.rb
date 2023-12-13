587a0126f72e8d04cf5d82617f5237c5d53f76eb
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-13 10:51:50 +0900

    検索結果がない場合

--- spec/system/facility_search_spec.rb
+++ spec/system/facility_search_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe '施設検索', type: :system do
   let!(:user) { FactoryBot.create(:user) }
   # テスト用の施設を作成
   let!(:facility) { FactoryBot.create(:facility, user: user, place_name: 'テスト施設') }
-  let!(:facility_hokkaido) { FactoryBot.create(:facility, user: user, prefecture: '北海道') }
+  let!(:hokkaido) { Prefecture.find_by(name: '北海道') }
+  let!(:facility_hokkaido) { FactoryBot.create(:facility, user: user, prefecture: hokkaido, place_name: '札幌の公園') }
 
   before do
     # 事前にサインインしておく
@@ -30,6 +31,12 @@ RSpec.describe '施設検索', type: :system do
   end
 
   it '存在しない施設名を入力すれば「お探しの施設はありません」と表示される' do
+    # 検索フォームに入力する
+    fill_in '施設名', with: 'フェイル施設'
+    click_on '検索'
+    # 検索結果欄と警告が表示されることを確認する
+    expect(page).to have_content '検索結果'
+    expect(page).to have_content 'お探しの施設はありません'
   end
 
   it '検索結果に関わらず、一覧表示には常にすべての施設が表示される' do
