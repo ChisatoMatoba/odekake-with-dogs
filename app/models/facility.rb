a7fae3c01ee23d28aebc34c7843fce24043fa95b
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-14 14:29:49 +0900

    バリデーション変更

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -9,7 +9,7 @@ class Facility < ApplicationRecord
   belongs_to :prefecture
   belongs_to :category
 
-  with_options numericality: { other_than: 1, message: 'を入力してください' } do
+  with_options numericality: { only_integer: true, message: 'を入力してください' } do
     validates :prefecture_id
     validates :category_id
   end
