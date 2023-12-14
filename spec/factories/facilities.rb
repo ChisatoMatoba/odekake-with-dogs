0064828558e26e2db7fd27fa5a9446f63333d152
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-14 15:23:47 +0900

    factorybot修正

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :facility do
-    prefecture { Prefecture.where.not(id: 1).sample }
+    prefecture { Prefecture.all.sample }
     place_name { Faker::Name.initials(number: 2) }
-    category { Category.where.not(id: 1).sample }
+    category { Category.all.sample }
     association :user
   end
 end
