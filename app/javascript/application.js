41aed61be93b2d4053ba542e916e424de70bbd8d
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-20 22:05:38 +0900

    セレクトボックス表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,4 +12,16 @@ import "jquery";
 window.jQuery = jQuery;
 window.$ = jQuery;
 
-import "select2"
+import "select2";
+// js-searchable クラスをつけたらSELECT2タグになるようにする
+document.addEventListener("turbo:load", () => {
+  $('.js-searchable').select2({
+    width: '100%',
+    allowClear: true,
+    theme: "bootstrap-5",
+		placeholder: "検索したいタグ名を3つまで選択して下さい",
+		maximumSelectionSize: 3,
+    // コンテナにすべてのCSSを適用
+    containerCssClass: ':all:'
+  });
+});
