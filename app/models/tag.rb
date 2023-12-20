cba2357546f108e97d5cb05191704a6c90db6d78
Chisato Matoba <chisato.matoba@luxiar.com>
2023-12-20 15:47:17 +0900

    ビュー

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :post_tags, dependent: :delete_all
   has_many :posts, through: :post_tags
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id id_value name updated_at]
+  end
 end
